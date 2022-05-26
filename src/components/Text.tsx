055a4da9f537d78f8eeb21efdab35a32d74472c4
Junhyuck Ko <mrbartrns@naver.com>
2022-05-27 08:52:55 +0900

    feat: create text

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,6 +1,46 @@
 interface TextProps {
-  children: string;
+  children: React.ReactNode;
+  mark?: boolean;
+  code?: boolean;
+  block?: boolean;
+  paragraph?: boolean;
+  strong?: boolean;
+  size?: number;
+  underline?: boolean;
+  del?: boolean;
+  color?: string;
+  [extraProps: string]: any;
 }
-export const Text = ({ children }: TextProps): JSX.Element => {
-  return <div>{children}</div>;
+
+// 일반적으로 size에 숫자가 아닌 large, medium, small
+// font-weight: regular, 400, 500, 600 등의 숫자를 이용하여 디자인 시스템을 구축한다.
+// 또한, inline-style-tag보다는 .css를 이용하여 클래스를 사용한다.
+export const Text = ({
+  children,
+  block = false,
+  paragraph = false,
+  strong = false,
+  size = 16,
+  underline = false,
+  del = false,
+  mark = false,
+  code = false,
+  color = 'black',
+  ...props
+}: TextProps): JSX.Element => {
+  const Tag = block ? 'div' : paragraph ? 'p' : 'span';
+  const fontStyle = {
+    fontWeight: strong ? 'bold' : undefined,
+    fontSize: size,
+    textDecoration: underline ? 'underline' : undefined,
+    color,
+  };
+  if (del) {
+    children = <del>{children}</del>;
+  } else if (mark) {
+    children = <mark>{children}</mark>;
+  } else if (code) {
+    children = <code>{children}</code>;
+  }
+  return <Tag style={{ ...fontStyle, ...props.style }}>{children}</Tag>;
 };
--- src/stories/Text.stories.tsx
+++ src/stories/Text.stories.tsx
@@ -4,6 +4,10 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 export default {
   title: 'Component/text',
   component: Text,
+  argTypes: {
+    size: { control: 'number' },
+    strong: { control: 'boolean' },
+  },
 } as ComponentMeta<typeof Text>;
 
 export const Default: ComponentStory<typeof Text> = (args) => (
