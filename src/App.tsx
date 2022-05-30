9b173656793f5d31ed5a718efbd9f163d32d4655
Junhyuck Ko <mrbartrns@naver.com>
2022-05-30 16:13:37 +0900

    chore: update boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
-import { css } from '@emotion/react';
-import TaskForm from '@components/TaskForm';
-import TaskList from '@components/TaskList';
-import Header from '@components/Header';
-import styled from '@emotion/styled';
-import TaskProvider from '@contexts/TaskProvider';
-
-const Container = styled.div`
-  width: 400px;
-  margin: 0 auto;
-`;
 
 const App = (): JSX.Element => (
   <div className="App">
-    <TaskProvider>
-      <Container>
-        <Header>TODO</Header>
-        <TaskForm />
-        <TaskList
-          css={css`
-            margin-top: 1rem;
-          `}
-        />
-      </Container>
-    </TaskProvider>
+    <h1>Hello, World!</h1>
   </div>
 );
 
