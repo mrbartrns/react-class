76acc6b17d889423d0d81d1ff8e8642f14d6d838
Junhyuck Ko <mrbartrns@naver.com>
2022-05-30 09:57:49 +0900

    feat: update Task

--- src/components/Task/index.tsx
+++ src/components/Task/index.tsx
@@ -14,7 +14,7 @@ const ListItem = styled.li`
   padding: 0 8px;
   border-radius: 16px;
   background-color: white;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0, 2);
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.2);
   list-style: none;
   box-sizing: border-box;
 `;
