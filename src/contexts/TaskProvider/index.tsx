165b3bf6b364c4d9e66b2127c96eaf01db76db4a
Junhyuck Ko <mrbartrns@naver.com>
2022-05-30 16:10:37 +0900

    feat: create useLocalStorage hooks

--- src/contexts/TaskProvider/index.tsx
+++ src/contexts/TaskProvider/index.tsx
@@ -1,4 +1,5 @@
-import { createContext, useState, useContext, useMemo } from 'react';
+import { createContext, useCallback, useContext, useMemo } from 'react';
+import useLocalStorage from '@hooks/useLocalStorage';
 import { v4 } from 'uuid';
 import { ITask } from '@/types/Task';
 
@@ -18,25 +19,35 @@ const TaskContext = createContext<ITaskContext>({} as unknown as ITaskContext);
 export const useTasks = () => useContext(TaskContext);
 
 const TaskProvider = ({ children }: TaskProviderProps) => {
-  const [tasks, setTasks] = useState<Array<ITask>>([]);
-  const addTask = (content: string) => {
-    setTasks((prev) => [...prev, { id: v4(), content, completed: false }]);
-  };
-  const updateTask = (id: string, completed: boolean) => {
-    setTasks((prev) =>
-      prev.map((item) =>
-        item.id === id ? { ...item, completed } : { ...item },
-      ),
-    );
-  };
+  const [tasks, setTasks] = useLocalStorage<Array<ITask>>('tasks', []);
 
-  const removeTask = (id: string) => {
-    setTasks((prev) => prev.filter((item) => item.id !== id));
-  };
+  const addTask = useCallback(
+    (content: string) => {
+      setTasks([...tasks, { id: v4(), content, completed: false }]);
+    },
+    [setTasks, tasks],
+  );
+  const updateTask = useCallback(
+    (id: string, completed: boolean) => {
+      setTasks(
+        tasks.map((item) =>
+          item.id === id ? { ...item, completed } : { ...item },
+        ),
+      );
+    },
+    [setTasks, tasks],
+  );
+
+  const removeTask = useCallback(
+    (id: string) => {
+      setTasks(tasks.filter((item) => item.id !== id));
+    },
+    [setTasks, tasks],
+  );
 
   const ret = useMemo(
     () => ({ tasks, addTask, updateTask, removeTask }),
-    [tasks],
+    [addTask, removeTask, tasks, updateTask],
   );
   return <TaskContext.Provider value={ret}>{children}</TaskContext.Provider>;
 };
