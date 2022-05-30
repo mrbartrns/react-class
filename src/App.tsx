49f407aec71bfe24d475a5b8417eff8d3920f073
Junhyuck Ko <mrbartrns@naver.com>
2022-05-30 09:06:29 +0900

    chore: update eslint rules

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-function App(): JSX.Element {
-  return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
-  );
-}
+const App = (): JSX.Element => (
+  <div className="App">
+    <h1>Hello, World!</h1>
+  </div>
+);
 
 export default App;
