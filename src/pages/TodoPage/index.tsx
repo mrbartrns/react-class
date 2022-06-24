946837ea927965c28df0cb8eb951da43b28348a5
Junhyuck Ko <mrbartrns@naver.com>
2022-06-24 14:00:39 +0900

    [refactor] 사용하지 않는 import 제거

--- src/pages/TodoPage/index.tsx
+++ src/pages/TodoPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState, useRef } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/store';
 import * as Task from '@/store/Tasks';
